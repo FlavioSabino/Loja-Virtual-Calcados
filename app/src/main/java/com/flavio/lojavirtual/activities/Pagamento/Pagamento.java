d9fbc40482a89c0d9610fdee68e3e5c29d1d1528
FlavioSabino <35316216+FlavioSabino@users.noreply.github.com>
2023-06-15 21:52:07 -0300

    finalizando class de pagamento

--- app/src/main/java/com/flavio/lojavirtual/activities/Pagamento/Pagamento.java
+++ app/src/main/java/com/flavio/lojavirtual/activities/Pagamento/Pagamento.java
@@ -1,11 +1,15 @@
 package com.flavio.lojavirtual.activities.Pagamento;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.flavio.lojavirtual.R;
 import com.flavio.lojavirtual.databinding.ActivityPagamentoBinding;
@@ -16,6 +20,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.mercadopago.android.px.configuration.AdvancedConfiguration;
+import com.mercadopago.android.px.core.MercadoPagoCheckout;
+import com.mercadopago.android.px.model.Payment;
+import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +38,7 @@ public class Pagamento extends AppCompatActivity {
     private String tamanho_calcado;
     private String nome;
     private String preco;
+
 
     private final String PUBLIC_KAY = "TEST-452675bf-46bb-48ca-8468-7975106b7263";
     private final String ACCESS_TOKEN = "TEST-8714136138335541-060521-3c3f2df30db5ecc461d9c72f74627c7f-286077356";
@@ -89,7 +98,7 @@ public class Pagamento extends AppCompatActivity {
     private  void criarPreferenciaPagamento(JsonObject dados){
 
         String site = "https://api.mercadopago.com";
-        String url = "/checkout/preferences?access_token" + ACCESS_TOKEN;
+        String url = "/checkout/preferences?access_token=" + ACCESS_TOKEN;
 
         Gson gson = new GsonBuilder().setLenient().create();
 
@@ -105,6 +114,7 @@ public class Pagamento extends AppCompatActivity {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 String preferenceId = response.body().get("id").getAsString();
+                criarPagamento(preferenceId);
             }
 
             @Override
@@ -113,4 +123,49 @@ public class Pagamento extends AppCompatActivity {
             }
         });
     }
+
+    private void criarPagamento(String preferenceId){
+        final AdvancedConfiguration advancedConfiguration =
+                new AdvancedConfiguration.Builder().setBankDealsEnabled(false).build();
+        new MercadoPagoCheckout
+                .Builder(PUBLIC_KAY, preferenceId)
+                .setAdvancedConfiguration(advancedConfiguration).build()
+                .startPayment(this, 123);
+    }
+
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 123) {
+            if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
+                final  Payment pagamento = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
+                respostaMercadoPago(pagamento);
+
+            } else if (resultCode == RESULT_CANCELED) {
+
+            } else {
+
+            }
+        }
+    }
+    private void respostaMercadoPago(Payment pagamento){
+        String status = pagamento.getPaymentStatus();
+
+        if(status.equalsIgnoreCase("approved")){
+            Snackbar snackbar = Snackbar.make(binding.container, "Sucesso ao fazer o pagamento", Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(ContextCompat.getColor(this,R.color.verde_800));
+            snackbar.setTextColor(Color.WHITE);
+            snackbar.show();
+
+        }else if (status.equalsIgnoreCase("rejected")){
+          Snackbar snackbar = Snackbar.make(binding.container, "Erro ao fazer o pagamento", Snackbar.LENGTH_SHORT);
+          snackbar.setBackgroundTint(Color.RED);
+          snackbar.setTextColor(Color.WHITE);
+          snackbar.show();
+
+        }
+    }
+
 }
